e6dbd26dbaa042c0078bb504d878054265fd8b85
Isra Abdelmobdy <progisra491@gmail.com>
2023-07-28 17:28:39 +0300

    Add project files.

---
+++ Lab3Q1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Lab3Q1
+{
+    class Point3D
+    {
+        public int X;
+        public int Y;
+        public int Z;
+
+        public Point3D() : this(1, 2, 3)
+        {
+            //X = 1;
+            //Y = 2;
+            //Z = 3;
+        }
+        public Point3D(int x) : this(x, 2, 3)
+        {
+            //X = x;
+            //Y = 2;
+            //Z = 3;
+        }
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"X Coordinate:{X}, Y Coordinate:{Y}, Z Coordinate:{Z}");
+        }
+
+        public static void Display(Point3D[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].Display();
+            }
+        }
+
+        public void ChangeCoordinates(int newX, int newY, int newZ)
+        {
+            X = newX;
+            Y = newY;
+            Z = newZ;
+        }
+
+        public override string ToString()
+        {
+            return $"X Coordinate:{X}, Y Coordinate:{Y}, Z Coordinate:{Z}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point3D point)
+            {
+                if (this.X == point.X && this.Y == point.Y && this.Z == point.Z)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point3D p1 = new Point3D(1);
+            Point3D p2 = new Point3D(1, 2, 3);
+            Console.Write("p1:  ");
+            p1.Display();
+            Console.Write("p2:  ");
+            p2.Display();
+
+
+            Console.WriteLine($"\nusing == operator: ");
+            if (p1 == p2)
+                Console.WriteLine("p1 and p2 are references to the same object");
+            else
+                Console.WriteLine("p1 and p2 are references to different objects");
+
+
+            Console.WriteLine($"\nusing Equals overriding function: ");
+            if (p1.Equals(p2))
+                Console.WriteLine($"p1 and p2 are equal");
+            else
+                Console.WriteLine($"p1 and p2 are not equal");
+
+            p1.ChangeCoordinates(4, 5, 6);
+            Console.WriteLine("\n\nAfter changing p1:");
+            Console.Write("p1:  ");
+            p1.Display();
+            Console.Write("p2:  ");
+            p2.Display();
+            p2 = p1;
+            Console.WriteLine("\n\nAfter writing p2 = p1 ");
+            Console.Write("p1:  ");
+            p1.Display();
+            Console.Write("p2:  ");
+            p2.Display();
+
+            Console.WriteLine($"\nusing == operator: ");
+            if (p1 == p2)
+                Console.WriteLine("p1 and p2 are references to the same object");
+            else
+                Console.WriteLine("p1 and p2 are references to different objects");
+
+
+            Point3D[] points = new Point3D[3] { new Point3D(7, 8, 9), new Point3D(10, 11, 12), new Point3D(13, 14, 15) };
+            Console.WriteLine($"\nDisplaying an array of points: ");
+            Point3D.Display(points);
+
+            Console.WriteLine($"\nTesting the ToString overriding function on points[3]: ");
+            Console.WriteLine(points[2].ToString());
+        }
+    }
+}
---
+++ Lab3Q2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lab3Q2
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public Person()
+        {
+            name = "";
+            age = 18;
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}";
+        }
+
+    }
+
+    class Employee : Person
+    {
+        public string NID { get; set; }
+        public double Salary { get; set; }
+        public Employee()
+        {
+            NID = string.Empty;
+            Salary = 0;
+        }
+        public Employee(string nID, double salary)
+        {
+            NID = nID;
+            Salary = salary;
+        }
+        public Employee(string name, int age, string nID, double salary) : base(name, age)
+        {
+            NID = nID;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}, National ID: {NID}, Salary: {Salary}";
+        }
+    }
+
+    class Trainee : Person
+    {
+        public string NID { get; set; }
+        public int IntakeNumber { get; set; }
+        public Trainee()
+        {
+            NID = string.Empty;
+            IntakeNumber = 0;
+        }
+        public Trainee(string nID, int intakeNumber)
+        {
+            NID = nID;
+            IntakeNumber = intakeNumber;
+        }
+        public Trainee(string name, int age, string nID, int intakeNumber) : base(name, age)
+        {
+            NID = nID;
+            IntakeNumber = intakeNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}, National ID: {NID}, Intake number: {IntakeNumber}";
+        }
+    }
+
+    internal class Program
+    {
+        public static string GetType(Person p)
+        {
+            return p.GetType().ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Person("Isra", 21),
+                new Employee("Isra", 21, "747584325535", 12000),
+                new Trainee("Momo", 4, "573746378382", 15),
+            };
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            //using Type object.GetType()
+            Person p1 = persons[1];
+            Type t1 = p1.GetType();
+            Console.WriteLine(t1);
+            //using the overload function: public static string GetType(Person p)
+            Person p2 = persons[2];
+            Console.WriteLine(GetType(p2));
+
+        }
+        
+    }
+}
